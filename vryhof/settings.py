62af1345d9eb1bb9e4ee222c122b242d166d794a
avryhof <amos@vryhofresearch.com>
2018-12-17 09:57:31 -0500

    Removed secret key from settings.

--- vryhof/settings.py
+++ vryhof/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=v5)cq7d%!6l7i2l33mlsamg)f^f#bb!6*=wrnj&5p#h**bpl-'
+SECRET_KEY = os.environ['VRYHOF_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
