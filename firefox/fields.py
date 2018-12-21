027fe8c183d21951af52a336d71665b5fadef699
avryhof <amos@vryhofresearch.com>
2018-12-21 07:59:58 -0500

    Make a version of the URLField that allows any length URL

---
+++ firefox/fields.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core import validators
+from django.db.models import TextField
+
+
+class LongURLField(TextField):
+    default_validators = [validators.URLValidator()]
+    description = _("URL")
+
+    def __init__(self, verbose_name=None, name=None, **kwargs):
+        super().__init__(verbose_name, name, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        return name, path, args, kwargs
+
+    def formfield(self, **kwargs):
+        # As with CharField, this will cause URL validation to be performed
+        # twice.
+        return super().formfield(**{
+            'form_class': forms.URLField,
+            **kwargs,
+        })
--- firefox/models.py
+++ firefox/models.py
@@ -1,11 +1,13 @@
 from django.db.models import Model, CharField, BooleanField, URLField, TextField, ForeignKey, DateTimeField, DO_NOTHING
 
+from firefox.fields import LongURLField
+
 
 class NewsFeed(Model):
     active = BooleanField(default=True)
     title = CharField(max_length=255, blank=True, null=True)
-    url = URLField(blank=True, null=True)
-    link = URLField(blank=True, null=True)
+    url = LongURLField(blank=True, null=True)
+    link = LongURLField(blank=True, null=True)
     description = TextField(blank=True, null=True)
 
     def __str__(self):
@@ -30,8 +32,8 @@ class NewsItem(Model):
     poster = ForeignKey(NewsImage, blank=True, null=True, on_delete=DO_NOTHING)
     abstract = TextField(blank=True, null=True)
     content = TextField(blank=True, null=True)
-    link = URLField(blank=True, null=True)
-    comments = URLField(blank=True, null=True)
+    link = LongURLField(blank=True, null=True)
+    comments = LongURLField(blank=True, null=True)
     guid = TextField(blank=True, null=True)
     date = DateTimeField(blank=True, null=True)
 
