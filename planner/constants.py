93170e0cf6a644a5c28bc3d0f6a113a739163f84
Amos Vryhof <amos@vryhofresearch.com>
2019-03-26 16:00:30 -0400

    Start defining the Budget model

---
+++ planner/constants.py
@@ -0,0 +1,10 @@
+BUDGET_DEBT = 'debt'
+BUDGET_EXPENSE = 'expense'
+BUDGET_INCOME = 'income'
+BUDGET_SAVINGS = 'savings'
+BUDGET_CHOICES = (
+    (BUDGET_DEBT, 'Debt'),
+    (BUDGET_EXPENSE, 'Expense'),
+    (BUDGET_INCOME, 'Income'),
+    (BUDGET_SAVINGS, 'Savings'),
+)
--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from planner.constants import BUDGET_CHOICES, BUDGET_EXPENSE
+
+
+class BudgetItem(models.Model):
+    item_type = models.CharField(max_length=32, choices=BUDGET_CHOICES, default=BUDGET_EXPENSE)
+    description
