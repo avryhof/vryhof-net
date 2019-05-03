dc969d9c41af6ef765118f18fd4c09dd9da7c120
Amos Vryhof <amos@vryhofresearch.com>
2019-05-03 13:03:24 -0400

    Convert to decorator-based approach

--- alexa/my_skill.py
+++ alexa/my_skill.py
@@ -1,89 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# This is a simple Hello World Alexa Skill, built using
+# the decorators approach in skill builder.
 import logging
 
 from ask_sdk_core.skill_builder import SkillBuilder
-from ask_sdk_core.handler_input import HandlerInput
-from ask_sdk_core.dispatch_components import AbstractRequestHandler, AbstractExceptionHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
-from ask_sdk_model import Response
 from ask_sdk_model.ui import SimpleCard
+
+sb = SkillBuilder()
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-sb = SkillBuilder()
+
+@sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
+def launch_request_handler(handler_input):
+    """Handler for Skill Launch."""
+    speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
+
+    return handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Hello World", speech_text)).set_should_end_session(
+        False).response
 
 
-class LaunchRequestHandler(AbstractRequestHandler):
-    """Handler for skill launch."""
+@sb.request_handler(can_handle_func=is_intent_name("HelloWorldIntent"))
+def hello_world_intent_handler(handler_input):
+    """Handler for Hello World Intent."""
+    speech_text = "Hello Python World from Decorators!"
 
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_request_type("LaunchRequest")(handler_input)
-
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-
-        logger.info(handler_input)
-
-        speech = "Hello, Dave!"
-        handler_input.response_builder.speak(speech)
-        return handler_input.response_builder.response
+    return handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Hello World", speech_text)).set_should_end_session(
+        True).response
 
 
-class HelloWorldIntentHandler(AbstractRequestHandler):
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("HelloWorldIntent")(handler_input)
+@sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
+def help_intent_handler(handler_input):
+    """Handler for Help Intent."""
+    speech_text = "You can say hello to me!"
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speech_text = "Hello, Dave!"
-
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(True)
-        return handler_input.response_builder.response
+    return handler_input.response_builder.speak(speech_text).ask(
+        speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).response
 
 
-class CancelAndStopIntentHandler(AbstractRequestHandler):
-    def can_handle(self, handler_input):
-        # type: (HandlerInput) -> bool
-        return is_intent_name("AMAZON.CancelIntent")(handler_input) or is_intent_name("AMAZON.StopIntent")(
-            handler_input)
+@sb.request_handler(
+    can_handle_func=lambda handler_input:
+    is_intent_name("AMAZON.CancelIntent")(handler_input) or
+    is_intent_name("AMAZON.StopIntent")(handler_input))
+def cancel_and_stop_intent_handler(handler_input):
+    """Single handler for Cancel and Stop Intent."""
+    speech_text = "Goodbye!"
 
-    def handle(self, handler_input):
-        # type: (HandlerInput) -> Response
-        speech_text = "So long, Dave."
-
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text))
-        return handler_input.response_builder.response
+    return handler_input.response_builder.speak(speech_text).set_card(
+        SimpleCard("Hello World", speech_text)).response
 
 
-class AllExceptionHandler(AbstractExceptionHandler):
-
-    def can_handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> bool
-        return True
-
-    def handle(self, handler_input, exception):
-        # type: (HandlerInput, Exception) -> Response
-        # Log the exception in CloudWatch Logs
-        print(exception)
-
-        speech = "I can't do that, Dave. Please try something else."
-        handler_input.response_builder.speak(speech).ask(speech)
-        return handler_input.response_builder.response
+@sb.request_handler(can_handle_func=is_intent_name("AMAZON.FallbackIntent"))
+def fallback_handler(handler_input):
+    """AMAZON.FallbackIntent is only available in en-US locale.
+    This handler will not be triggered except in that locale,
+    so it is safe to deploy on any locale.
+    """
+    speech = (
+        "The Hello World skill can't help you with that.  "
+        "You can say hello!!")
+    reprompt = "You can say hello!!"
+    handler_input.response_builder.speak(speech).ask(reprompt)
+    return handler_input.response_builder.response
 
 
-# Other skill components here ....
+@sb.request_handler(can_handle_func=is_request_type("SessionEndedRequest"))
+def session_ended_request_handler(handler_input):
+    """Handler for Session End."""
+    return handler_input.response_builder.response
 
-# Register all handlers, interceptors etc.
-# For eg : sb.add_request_handler(LaunchRequestHandler())
 
-sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(HelloWorldIntentHandler())
-sb.add_request_handler(CancelAndStopIntentHandler())
+@sb.exception_handler(can_handle_func=lambda i, e: True)
+def all_exception_handler(handler_input, exception):
+    """Catch all exception handler, log exception and
+    respond with custom message.
+    """
+    logger.error(exception, exc_info=True)
 
-sb.add_exception_handler(AllExceptionHandler())
+    speech = "Sorry, there was some problem. Please try again!!"
+    handler_input.response_builder.speak(speech).ask(speech)
+
+    return handler_input.response_builder.response
+
 
 skill = sb.create()
