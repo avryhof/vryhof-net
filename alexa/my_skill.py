c1dc5c99412f71a09ba200ca8101351c53fd55ff
Amos Vryhof <amos@vryhofresearch.com>
2019-05-16 14:29:21 -0400

    Read slot value from right place

--- alexa/my_skill.py
+++ alexa/my_skill.py
@@ -45,9 +45,9 @@ def get_story_intent_handler(handler_input):
 
 @sb.request_handler(can_handle_func=is_intent_name('ReadStoryIntent'))
 def read_story_intent_handler(handler_input):
-    attr = handler_input.attributes_manager.session_attributes
+    slots = handler_input.request_envelope.request.intent.slots
 
-    story_title = attr.get("story_title")
+    story_title = slots.get("story_title")
     speech_text = get_story(story_title=story_title)
 
     return handler_input.response_builder.speak(speech_text).set_card(
