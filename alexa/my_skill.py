6fdb5d1799ff40473137bca1c4f8ea8b7d220560
Amos Vryhof <amos@vryhofresearch.com>
2019-05-17 09:22:30 -0400

    Look at slot as a dict to get value.

--- alexa/my_skill.py
+++ alexa/my_skill.py
@@ -47,7 +47,7 @@ def get_story_intent_handler(handler_input):
 def read_story_intent_handler(handler_input):
     slots = handler_input.request_envelope.request.intent.slots
 
-    story_title = slots.story_title.value
+    story_title = slots.get('story_title').value
     logger.info(story_title)
     speech_text = get_story(story_title=story_title)
 
