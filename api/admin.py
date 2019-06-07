3d11cd864ee0d47b8276ea69907dc0ffd077b1e7
Amos Vryhof <amos@vryhofresearch.com>
2019-06-07 15:10:30 -0400

    Search By Zip Code

--- api/admin.py
+++ api/admin.py
@@ -7,18 +7,18 @@ from .models import PostalCode
 @admin.register(PostalCode)
 class PostalCodeAdmin(admin.ModelAdmin):
     list_display = (
-        'country_code',
-        'postal_code',
-        'place_name',
-        'admin_name1',
-        'admin_code1',
-        'admin_name2',
-        'admin_code2',
-        'admin_name3',
-        'admin_code3',
-        'latitude',
-        'longitude',
-        'accuracy',
+        "country_code",
+        "postal_code",
+        "place_name",
+        "admin_name1",
+        "admin_code1",
+        "admin_name2",
+        "admin_code2",
+        "admin_name3",
+        "admin_code3",
+        "latitude",
+        "longitude",
+        "accuracy",
     )
-    list_filter = ('admin_name1',)
-    search_fields = ("place_name", 'admin_name1', 'admin_code1', 'admin_name2',)
+    list_filter = ("admin_name1",)
+    search_fields = ("place_name", "postal_code", "admin_name1", "admin_code1", "admin_name2")
