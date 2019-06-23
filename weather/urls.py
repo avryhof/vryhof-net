aa30b4c024bfb6a7c2d573b932098ebda388bab1
Amos Vryhof <amos@vryhofresearch.com>
2019-06-23 05:35:19 -0400

    Added camera image to weather station page

--- weather/urls.py
+++ weather/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from weather.views import WeatherView, weather_image
 
 urlpatterns = [
-    url(r"^image.jpg$", weather_image, name="firefox_home"),
-    url(r"^(?P<date>\d{4}\d{2}\d{2})/image.jpg$", weather_image, name="firefox_home"),
-    url(r"^(?P<date>\d{8})/(?P<time>\d{6})/image.jpg$", weather_image, name="firefox_home"),
+    url(r"^image.jpg$", weather_image, name="weather_image"),
+    url(r"^(?P<date>\d{4}\d{2}\d{2})/image.jpg$", weather_image, name="weather_image_date"),
+    url(r"^(?P<date>\d{8})/(?P<time>\d{6})/image.jpg$", weather_image, name="weather_image_time"),
     url(r"^$", WeatherView.as_view(), name="firefox_home")
 ]
