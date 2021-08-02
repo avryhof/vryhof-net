6a7c79a10b17082c20a09db1703d2202ef678687
Amos  Vryhof <amos@vryhofresearch.com>
2021-08-02 07:47:17 -0400

    Check in endpoint tester just to keep in sync

--- catalog/management/commands/test-endpoint.py
+++ catalog/management/commands/test-endpoint.py
@@ -3,6 +3,8 @@ import uuid0
 from django.core.management import BaseCommand
 from django.urls import reverse
 
+from utilities.debugging import log_message
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -10,6 +12,8 @@ class Command(BaseCommand):
         endpoint_path = reverse("rfid-lookup")
 
         endpoint_url = "http://127.0.0.1:8000{}".format(endpoint_path)
+
+        log_message("talking to: {}".format(endpoint_url))
 
         resp = requests.post(endpoint_url, data=dict(rfid_id=rfid_id))
 
