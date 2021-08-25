7624e6a41e9568149418fc564a82c1619453b5dc
Amos  Vryhof <amos@vryhofresearch.com>
2021-08-25 07:09:46 -0400

    Fix requirements so weather station page doesn't crash

--- geocaching/templatetags/geocaching.py
+++ geocaching/templatetags/geocaching.py
@@ -6,10 +6,11 @@ register = template.Library()
 
 static_root = settings.STATIC_URL
 
+
 @register.simple_tag
-def waypoint_marker(map_prefix='leaflet', latitude=None, longitude=None, waypoint_name=None):
-    map_id = '%s_map' % map_prefix
-    coords = 'var %s_marker_coords = [%s, %s];' % (waypoint_name, latitude, longitude)
-    code = 'L.marker(%s_marker_coords).addTo(%s);' % (waypoint_name, map_id)
+def waypoint_marker(map_prefix="leaflet", latitude=None, longitude=None, waypoint_name=None):
+    map_id = "%s_map" % map_prefix
+    coords = "var %s_marker_coords = [%s, %s];" % (waypoint_name, latitude, longitude)
+    code = "L.marker(%s_marker_coords).addTo(%s);" % (waypoint_name, map_id)
 
     return render_javascript_code([coords, code])
