d5f253cfd11becaa2b0571aadb9b207ffbb54641
Amos Vryhof <amos@vryhofresearch.com>
2022-06-03 14:15:14 -0400

    Update signature endpoint

--- firefox/urls.py
+++ firefox/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from firefox.views import FirefoxHomeView
+from firefox.views import FirefoxHomeView, SignatureView
 
-urlpatterns = [path("", FirefoxHomeView.as_view(), name="firefox_home")]
+urlpatterns = [
+    path("", FirefoxHomeView.as_view(), name="firefox_home"),
+    path("signature/", SignatureView.as_view(), name="signature"),
+]
--- firefox/views.py
+++ firefox/views.py
@@ -65,3 +65,32 @@ class FirefoxHomeView(TemplateView):
     @method_decorator(csrf_protect)
     def dispatch(self, *args, **kwargs):
         return super(FirefoxHomeView, self).dispatch(*args, **kwargs)
+
+
+class SignatureView(TemplateView):
+    extra_css = ["css/ff-style.css"]
+    extra_javascript = []
+
+    template_name = "email-signature.html"
+    name = "Email Signature"
+
+    request = None
+
+    def get_context_data(self, **kwargs):
+        context = super(SignatureView, self).get_context_data(**kwargs)
+        context["page_title"] = self.name
+        context["extra_css"] = self.extra_css
+        context["extra_javascript"] = self.extra_javascript
+        context["request"] = self.request
+
+        return context
+
+    def get(self, request, *args, **kwargs):
+        self.request = request
+        context = self.get_context_data()
+
+        return render(request, self.template_name, context)
+
+    @method_decorator(csrf_protect)
+    def dispatch(self, *args, **kwargs):
+        return super(SignatureView, self).dispatch(*args, **kwargs)
