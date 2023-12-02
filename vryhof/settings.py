433650cec25a98a303e3288f641687f2a0a50d7d
Amos Vryhof <avryhof@gmail.com>
2023-12-02 13:59:25 -0500

    allauth middleware

--- vryhof/settings.py
+++ vryhof/settings.py
@@ -88,6 +88,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.locale.LocaleMiddleware",
+     "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "vryhof.urls"
