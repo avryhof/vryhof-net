806226761a6fd0c3d7cb2ad55a81b08fbda410a2
Amos Vryhof <avryhof@gmail.com>
2024-03-09 19:47:19 -0500

    Add admin for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,22 @@
+from django.contrib import admin
+
+from accounts.models import EmailDomain, AuthSession, UserPrefs
+
+
+@admin.register(EmailDomain)
+class EmailDomainAdmin(admin.ModelAdmin):
+    list_display = ["domain", "site", "enabled"]
+    list_editable = ["enabled"]
+
+
+@admin.register(AuthSession)
+class AuthSessionAdmin(admin.ModelAdmin):
+    list_display = ["user", "is_authenticated", "token", "expires_at"]
+
+
+@admin.register(UserPrefs)
+class UserPrefsAdmin(admin.ModelAdmin):
+    list_display = ["user"]
 
 
 
