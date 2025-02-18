866b7e0c7aac84cc0ffb5642e7ce2c215341cf73
Amos Vryhof <avryhof@gmail.com>
2025-02-18 18:28:20 -0500

    fix missing import

--- weather/admin.py
+++ weather/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-from .models import WeatherStation, WeatherData, NWSPoint, NWSForecast
+from .models import WeatherStation, WeatherData, NWSPoint, NWSForecast, WeatherImages
 
 
 class WeatherStationResource(resources.ModelResource):
